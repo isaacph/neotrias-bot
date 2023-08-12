6ac38db5220d75471545dc9fe8e29fb14c863d57
isaacph <isaacphuffman@gmail.com>
2023-08-12 23:17:24 +0000

    Update comment on server address to use

--- public_key_sample.py
+++ public_key_sample.py
@@ -2,6 +2,6 @@
 # PUBLIC_KEY = "big hash"
 # APPLICATION_ID = "like 20 digit number"
 # AUTH_TOKEN = "Bot big-base64-with-dashes"
-# SERVER_HOSTNAME = "minecraft server address"
+# SERVER_HOSTNAME = "minecraft server address, probably the private IP if the bot is on the same VPC as the server"
 # SERVER_RCON_PORT = (int) the RCON port in server.properties
 # SERVER_RCON_PASSWORD = "RCON password in server.properties"
